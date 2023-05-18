abd8dab691b0daff00b3c3e283f405cb66db5dcc
Uttej Venkata Sastry <uttejvsk@hotmail.com>
2023-05-18 22:19:21 +0530

    Part 1 complete

--- part1.12-1.14/src/App.jsx
+++ part1.12-1.14/src/App.jsx
@@ -1,103 +1,93 @@
-import { useState } from "../../part1.6-1.11/node_modules/@types/react";
-
-
-const Button = ({ handleClick, text }) => {
-  return (
-    <button onClick={handleClick}>{text}</button>
-  )
-
-}
-
-const StatisticsLine = ({ text, value }) => {
-  return (
-    <tr>
-      <td>
-        {text}:
-      </td>
-      <td>{value}</td>
-    </tr>
-  )
-}
-
-const Statistics = ({ feedback, total }) => {
-  const { good, neutral, bad } = feedback
-  const average = total ? ((good - bad) / total).toFixed(2) : 0
-  const positivePercent = total ? ((good / total) * 100).toFixed(2) : 0;
-  if (total === 0) {
-    return (
-      <>
-        <h1>Statistics</h1>
-        <p>No Feedback provided</p>
-      </>
-    )
-  }
-
-  return (
-    <>
-      <h1>
-        Statistics
-      </h1>
-      <table>
-        <tbody>
-
-          <StatisticsLine text='Good' value={good} />
-          <StatisticsLine text='Neutral' value={neutral} />
-          <StatisticsLine text='Bad' value={bad} />
-          <StatisticsLine text='total' value={total} />
-          <StatisticsLine text='Average' value={average} />
-          <StatisticsLine text='positive Percent' value={`${positivePercent}%`} />
-        </tbody>
-      </table>
-    </>
-  )
-}
+import { useState } from 'react';
 
 const App = () => {
-  const [feedback, setFeedback] = useState({
-    good: 0,
-    neutral: 0,
-    bad: 0
-  })
-  const [total, setTotal] = useState(0)
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 10 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when diagnosing patients.',
+    'The only way to go fast is to go well.',
+  ];
 
-  const handleGoodClick = () => {
-    setFeedback({
-      ...feedback,
-      good: feedback.good + 1
+  const [selected, setSelected] = useState(0);
+  const [vote, setVote] = useState(0);
+  const [saveVote, setSaveVote] = useState([]);
+
+  const getRandomAnecdote = () => {
+    return Math.floor(Math.random() * anecdotes.length);
+  };
+
+  const randomAnectode = () => {
+    const rand = getRandomAnecdote();
+    console.log(rand)
+    const selectedAnecdote = saveVote.find(vote => vote.id === rand)
+
+    if (selectedAnecdote) {
+      setSelected(selectedAnecdote.id);
+      setVote(selectedAnecdote.vote);
+    } else {
+      setSelected(rand);
+      setVote(0);
+    }
+  };
+
+  const voteAnectode = () => {
+    const updatedVotes = saveVote.map(vote => {
+      if (vote.id === selected) {
+        return { ...vote, vote: vote.vote + 1 };
+      }
+      return vote;
+    });
+
+    if (saveVote.find(vote => vote.id === selected)) {
+      setSaveVote(updatedVotes);
+    } else {
+      setSaveVote([...saveVote, { id: selected, vote: 1 }]);
+    }
+
+    setVote(vote + 1);
+  };
+
+  const getAnecdoteWithHighestVotes = () => {
+    if (saveVote.length === 0) {
+      return null;
+    }
+
+    const anecdoteWithHighestVotes = saveVote.reduce((maxVote, vote) => {
+      if (vote.vote > maxVote.vote) {
+        return vote
+      }
+      return maxVote
     })
-    setTotal(total + 1)
+
+    const anecdoteIndex = anecdotes.findIndex(
+      (_, index) => index === anecdoteWithHighestVotes.id
+    )
+    return { anecdote: anecdotes[anecdoteIndex], votes: anecdoteWithHighestVotes.vote }
   }
 
-  const handleNeutralClick = () => {
-    setFeedback({
-      ...feedback,
-      neutral: feedback.neutral + 1
-    })
-    setTotal(total + 1)
-
-  }
-
-  const handleBadClick = () => {
-    setFeedback({
-      ...feedback,
-      bad: feedback.bad + 1
-    })
-    setTotal(total + 1)
-
-  }
+  const anecdoteWithHighestVotes = getAnecdoteWithHighestVotes()
 
   return (
     <div>
-      <h1>
-        Give Feedback
-      </h1>
-      <Button handleClick={handleGoodClick} text='good' />
-      <Button handleClick={handleNeutralClick} text='neutral' />
-      <Button handleClick={handleBadClick} text='bad' />
-      <Statistics feedback={feedback} total={total} />
-    </div>
-  )
-}
+      <p>{anecdotes[selected]}</p>
+      <p>Votes: {vote}</p>
+      <button onClick={randomAnectode}>Next Anecdote</button>
+      <button onClick={voteAnectode}>Vote</button>
 
+
+      {anecdoteWithHighestVotes && (
+        <p>
+          {anecdoteWithHighestVotes.anecdote}
+          <p>Total Votes:{anecdoteWithHighestVotes.votes}</p>
+
+        </p>
+      )}
+    </div>
+  );
+};
 
 export default App;
--- part1.12-1.14/src/main.jsx
+++ part1.12-1.14/src/main.jsx
@@ -1,5 +1,5 @@
-import React from '../../part1.6-1.11/node_modules/@types/react'
-import ReactDOM from '../../part1.6-1.11/node_modules/@types/react-dom/client'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 import App from './App'
 
 
